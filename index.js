e5df043f4151abdfb126d39f31d1916b514d3ed4
Mihir Chaturvedi <plibither8@gmail.com>
2020-04-16 20:14:53 +0530

    Init: Add initial files

---
+++ index.js
@@ -0,0 +1,3 @@
+const pptr = require('puppeteer');
+
+const LOGIN_URL = 'https://accounts.fitbit.com/login?targetUrl=https%3A%2F%2Fwww.fitbit.com%2Flogin%2Ftransferpage%3Fredirect%3Dhttps%253A%252F%252Fwww.fitbit.com&lcl=en_IN';
